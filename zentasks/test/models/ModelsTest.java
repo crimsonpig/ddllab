2eb8ef2d59296ea0f5336f7610a5fa1e7785441b
keith.stabbert <keith.stabbert@yahoo.com>
2013-09-21 20:21:41 -0700

    Finished page 4

--- zentasks/test/models/ModelsTest.java
+++ zentasks/test/models/ModelsTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class ModelsTest extends WithApplication {
 	@Before
 	public void setUp() {
-		start(fakeApplication(inMemoryDatabase()));
+		start(fakeApplication(inMemoryDatabase(), fakeGlobal()));
 	}
 
 	@Test
