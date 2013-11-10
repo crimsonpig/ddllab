6c3052f3f57b13f5b748d09971a5f2e1f25b4b26
crimsonpig <keith.stabbert@yahoo.com>
2013-11-10 01:08:41 -0800

    Added some more comments.

--- csc174/src/stabbert_Keith_hw3.java
+++ csc174/src/stabbert_Keith_hw3.java
@@ -4,8 +4,8 @@ public class stabbert_Keith_hw3 {
 	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	
 	public static void main(String[] args){
-//		String host = "athena.ecs.csus.edu";
-		String host = "localhost";
+		String host = "athena.ecs.csus.edu";
+//		String host = "localhost";
 		String databaseUrl = "jdbc:mysql://"+host+"/cs174127";
 		String user = "cs174127";
 		String password = "bolsxrfn";
@@ -28,31 +28,37 @@ public class stabbert_Keith_hw3 {
 			Connection conn = DriverManager.getConnection(url, username, password);
 			System.out.println("Connection open to database at: "+url);
 			Statement stmt1 = conn.createStatement();
+			System.out.println("Listing all persons who are both employees and customers of the bank.");
 			ResultSet rs1 = stmt1.executeQuery(QUERY_1);
 			System.out.println("Query 1 executed");
 			while(rs1.next()){
-				System.out.println("name: "+rs1.getString("name"));
+				System.out.println("\nname: "+rs1.getString("name"));
 				System.out.println("taxPayerID: "+rs1.getString("taxPayerID"));
 			}
 			rs1.close();
 			stmt1.close();
 			
 			Statement stmt2 = conn.createStatement();
+			System.out.println("\nQuerying \"empLoan\" view. \n" +
+					"Listing all persons who are both employees and customers of the bank.");
 			ResultSet rs2 = stmt2.executeQuery(QUERY_2);
-			System.out.println("\nQuery 2 executed");
+			System.out.println("Query 2 executed");
 			while(rs2.next()){
-				System.out.println("taxPayerID: "+rs2.getString("taxPayerID"));
+				System.out.println("\ntaxPayerID: "+rs2.getString("taxPayerID"));
 				System.out.println("name: "+rs2.getString("name"));
 				System.out.println("salary: "+rs2.getString("salary"));
 			}
 			rs2.close();
 			stmt2.close();
 			
+			System.out.println("\nPreparing CallableStatement to call \"get_no_of_tran\" function.");
 			CallableStatement cstmt3 = conn.prepareCall("{?= call get_no_of_tran(?)}");
 			cstmt3.registerOutParameter(1,Types.NUMERIC);
 			cstmt3.setString(2,"123456789");
+			System.out.println("Calling get_no_of_tran(\"123456789\") ");
 			cstmt3.execute();
-			System.out.println("\nno of transactions = "+cstmt3.getInt(1));
+			System.out.println("\nnumber of transactions for account \"123456789\" = "+cstmt3.getInt(1)+" transactions");
+			cstmt3.close();
 			
 			conn.close();
 			System.out.println("\nDisconnected from database at: "+url);
